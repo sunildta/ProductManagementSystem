5516545f40023b2cff64d4cc99489732c7cac5f6
Sunildta <sunil1u89@gmail.com>
2026-06-27 23:53:10 +0530

    feat: add ValidationService using Data Annotations

---
+++ Validation/IValidationService.cs
@@ -0,0 +1,16 @@
+﻿using ProductManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductManagementSystem.Validation
+{
+    public interface IValidationService
+    {
+        ///<summary>
+        ///validate product using data annotation, return true if valid
+        ///else populates<paramref name="error"/>
+        ///</summary>
+        bool Validate(Product product, out IReadOnlyList<string> errors);
+    }
+}
---
+++ Validation/ValidationService.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ProductManagementSystem.Models;
+
+namespace ProductManagementSystem.Validation
+{
+    public class ValidationService : IValidationService
+    {
+        public bool Validate(Product product, out IReadOnlyList<string> errors)
+        {
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(product);
+            var isValid = Validator.TryValidateObject(product, context, results, validateAllProperties: true);
+
+            errors = results.Select(r => r.ErrorMessage ?? "unknown error.").ToList();
+            return isValid;
+        }
+    }
+
+}
